9b109ee214e79124c5552ae62db7eec3291dd53f
Liang Chen <liangyu.chen.dev@gmail.com>
2024-12-08 19:26:39 -0500

    refactor add function to improve instruction handling and job retrieval logic

--- runner/service_layer/services.py
+++ runner/service_layer/services.py
@@ -2,6 +2,7 @@ import uuid
 from typing import List, Tuple, Dict
 from runner.domain import model, events
 from runner.service_layer import unit_of_work
+from runner.adapters import job_repository
 
 
 class JobHasCompleted(Exception):
@@ -9,12 +10,17 @@ class JobHasCompleted(Exception):
 
 
 def add(
-    instructions: List[Tuple[str, Dict]],
+    raw_instructions: List[Tuple[str, Dict]],
     job_uow: unit_of_work.AbstractJobUnitOfWork,
 ):
     jobid = str(uuid.uuid4())
+    instructions = [model.Instruction(script, arg) for script, arg in raw_instructions]
     with job_uow:
-        while job_uow.jobs.get(jobid):
+        while True:
+            try:
+                job = job_uow.jobs.get(jobid)
+            except job_repository.JobNotFound:
+                break
             jobid = str(uuid.uuid4())
         job = model.Job(jobid, instructions, model.JobStates.Init)
         job_uow.jobs.add(job)
